5017576404cefdd3bbb9b3f53580b2634247b1cf
DannyGarcia <DannyGarcia@DESKTOP-6PV6HSF>
2019-09-13 18:48:08 -0600

    Se aplico plantilla para sitio web admin, Layouts admin y login. Tambien filtros y ordenamientos para registros activos.  Por ultimo se aplico el reporte para mostrar en ventana windows

---
+++ ServiciosTecnicos/ServiciosTecnicos.WebAdmin/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServiciosTecnicos.WebAdmin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View(); //Este no hara nada
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection data)
+        {
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
